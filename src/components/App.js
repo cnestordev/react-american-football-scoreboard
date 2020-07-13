502334d11775d9506b10c072aff084a2db64c67f
Nestor <ncervan87@gmail.com>
2020-07-13 13:47:10 -0700

    stretch goal add quarter, add useEffect for timer, break into smallter components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BottomRow from "./BottomRow";
 import TopRow from './TopRow'
 import Buttons from './Buttons'
@@ -8,10 +8,26 @@ function App() {
   const [homeScore, setHomeScore] = useState(0)
   const [awayScore, setAwayScore] = useState(0)
   const [quarter, setQuarter] = useState(1)
+  const [timer, setTimer] = useState(5)
+  const [id, setId] = useState('')
+  let x;
+
+  useEffect(() => {
+    if (timer === 5) {
+      let intervalId = setInterval(() => {
+        setId(intervalId)
+        setTimer(prevTimer => prevTimer - 1)
+      }, 1000);
+    }
+    if (timer === 0) {
+      clearInterval(id)
+    }
+  }, [timer])
+
   return (
     <div className="container">
       <section className="scoreboard">
-        <TopRow home={homeScore} away={awayScore} />
+        <TopRow timer={timer} home={homeScore} away={awayScore} />
         <BottomRow value={quarter} increment={setQuarter} />
       </section>
       <Buttons
@@ -21,6 +37,7 @@ function App() {
         awayField={() => setAwayScore(awayScore + 3)}
         quarter={() => setQuarter(quarter + 1)}
       />
+      <button onClick={() => console.log(timer)}>x</button>
     </div>
   );
 }
--- src/components/TopRow.js
+++ src/components/TopRow.js
@@ -9,7 +9,7 @@ function TopRow(props) {
                 {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
                 <div className="home__score">{props.home}</div>
             </div>
-            <div className="timer">00:03</div>
+            <div className="timer">{props.timer}</div>
             <div className="away">
                 <h2 className="away__name">Tigers</h2>
                 <div className="away__score">{props.away}</div>
